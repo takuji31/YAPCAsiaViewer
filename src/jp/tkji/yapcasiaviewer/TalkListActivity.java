b40916e044d8ae14f4585b5c52a71d55bedc090f
Nishibayashi Takuji <takuji31@gmail.com>
2012-08-25 23:30:41 +0900

    不要なimport

--- src/jp/tkji/yapcasiaviewer/TalkListActivity.java
+++ src/jp/tkji/yapcasiaviewer/TalkListActivity.java
@@ -3,8 +3,6 @@ package jp.tkji.yapcasiaviewer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NavUtils;
-import android.view.MenuItem;
 
 public class TalkListActivity extends FragmentActivity
         implements TalkListFragment.Callbacks {
