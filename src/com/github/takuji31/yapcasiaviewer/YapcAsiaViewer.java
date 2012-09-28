743ded7d1f2f0bafc9ca2d9a9d82012f75e20791
Nishibayashi Takuji <takuji31@gmail.com>
2012-09-28 15:22:28 +0900

    トーク開始5分前にnotifyで通知するようにした

--- src/com/github/takuji31/yapcasiaviewer/YapcAsiaViewer.java
+++ src/com/github/takuji31/yapcasiaviewer/YapcAsiaViewer.java
@@ -1,5 +1,7 @@
 package com.github.takuji31.yapcasiaviewer;
 
+import java.util.List;
+
 import com.github.takuji31.yapcasiaviewer.R;
 
 import com.github.takuji31.appbase.app.BaseApp;
@@ -7,6 +9,17 @@ import com.github.takuji31.appbase.app.BaseApp;
 public class YapcAsiaViewer extends BaseApp {
 	
 	private static final int sPreferenceVersion = 1;
+	public static final String BUNDLE_KEY_TALK  = "talk";
+	public static final String ACTION_NOTIFY = "com.github.takuji31.yapcasiaviewer.notify";
+	
+	public static List<Talk> talkList;
+	
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		talkList = Talk.getCheckList(this);
+	}
 	
 	@Override
 	public int getDefaultPrefVersion() {
